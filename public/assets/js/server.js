7d1ea70b5a1dbe99cb107fcda57eaa48d1a05f2f
spreston <sam.preston11@gmail.com>
2021-07-18 17:29:24 -0500

    Returns 'db.json' data from '/api/notes'.

--- public/assets/js/server.js
+++ public/assets/js/server.js
@@ -1,6 +1,7 @@
 // Required dependencies
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 
 // Set up express
 const app = express();
@@ -9,7 +10,19 @@ const PORT = 3000;
 // Basic route
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '../../notes.html')));
 
+app.get('/api/notes', (req, res) => {
+    
+    const allNotes = getDb('db.json');
+    res.end(allNotes);
+});
+
 app.get('*', (req, res) => res.sendFile(path.join(__dirname, '../../index.html')));
 
 // Start server
 app.listen(PORT, () => console.log(`App is listening on port: ${PORT}`));
+
+// Function 'getDb' reads and returns 
+const getDb = (name) => {
+
+    return fs.readFileSync(path.join(__dirname, `../../../db/${name}`));
+}
