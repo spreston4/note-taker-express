43fc71bdbda66ee5f456f0d8d73c9e181c625ca9
spreston <sam.preston11@gmail.com>
2021-07-18 16:45:50 -0500

    Get'/' paths to index.html.

--- public/assets/js/server.js
+++ public/assets/js/server.js
@@ -1,15 +1,14 @@
 // Required dependencies
 const express = require('express');
+const path = require('path');
 
 // Set up express
 const app = express();
 const PORT = 3000;
 
 // Basic route
-app.get('/', function (req, res) {
-    res.send('hello world')
-  });
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '../../index.html')));
+   
 
 // Start server
-
 app.listen(PORT, () => console.log(`App is listening on port: ${PORT}`));
