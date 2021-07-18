e36a6189c4071665ca5592c69fbd85ed339baf9c
spreston <sam.preston11@gmail.com>
2021-07-18 16:54:01 -0500

    Path for notes.html functioning.

--- public/assets/js/server.js
+++ public/assets/js/server.js
@@ -7,8 +7,9 @@ const app = express();
 const PORT = 3000;
 
 // Basic route
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, '../../index.html')));
-   
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '../../notes.html')));
+
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, '../../index.html')));
 
 // Start server
 app.listen(PORT, () => console.log(`App is listening on port: ${PORT}`));
