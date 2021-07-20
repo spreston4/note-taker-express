dad02d0e4e6bc59bbd695165b65d62d422230a54
spreston <sam.preston11@gmail.com>
2021-07-19 20:34:03 -0500

    Basics for POST route started.

--- public/assets/js/server.js
+++ public/assets/js/server.js
@@ -5,24 +5,56 @@ const fs = require('fs');
 
 // Set up express
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
-// Basic route
-app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '../../notes.html')));
-
-app.get('/api/notes', (req, res) => {
-    
-    const allNotes = getDb('db.json');
-    res.end(allNotes);
-});
-
-app.get('*', (req, res) => res.sendFile(path.join(__dirname, '../../index.html')));
+// Set up middleware
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
 
 // Start server
 app.listen(PORT, () => console.log(`App is listening on port: ${PORT}`));
 
+// Notes.html route
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '../../notes.html')));
+
+// Route to return notes data from db.json
+app.get('/api/notes', (req, res) => {
+    
+    // const allNotes = getDb('db.json');
+
+    // res.writeHead(200, { 'Content-Type': 'application/json' });
+
+    // res.end(allNotes);
+
+    res.sendFile(path.join(__dirname, '../../../db/db.json'));
+});
+
+app.post('/api/notes', (req, res) => {
+
+    const newNote = {
+        "title": req.body.title,
+        "text": req.body.text
+    };
+    
+    console.log('New Note');
+    console.log(newNote);
+    // // Get current notes
+    // fs.readFile(path.join(__dirname, '../../../db/db.json'), 'utf8', (err, data) => {
+    //     // Append new note to previous noites
+    
+    //     // save newly extended collection back to bd
+    // });
+
+    res.send('POST NOTES');
+});
+
+// Catch all route
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, '../../index.html')));
+
 // Function 'getDb' reads and returns 
 const getDb = (name) => {
 
-    return fs.readFileSync(path.join(__dirname, `../../../db/${name}`));
+    return fs.readFileSync(path.join(__dirname, `../../../db/${name}`), 'utf8', (err, data) => (err) ? console.error(err) : data);
 }
+
+
