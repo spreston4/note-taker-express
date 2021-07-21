0766371d6d1da0c767a0119e54eff2224b9ce931
spreston <sam.preston11@gmail.com>
2021-07-21 18:28:49 -0500

    Get route for api/notes added. Now displays list of notes on notes.html.

--- routes/notes.js
+++ routes/notes.js
@@ -1,9 +1,11 @@
 // Establish required dependancies
 const notes = require('express').Router();
+const { readFromFile, writeToFile, readAndAppend} = require('../helpers/utilities');
 
 // GET route to retrieve all notes
 notes.get('/', (req, res) => {
-    res.send('GET NOTE');
+    
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 });
 
 // POST route to to create a new note
